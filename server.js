022665af33403f50b504c9e59445a8a33bd80723
IgnatiusFrancis <ignatiuzzfrank@gmail.com>
2023-10-26 22:43:34 +0100

    Fixed cors error

--- server.js
+++ server.js
@@ -7,6 +7,28 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const { databaseConnection } = require("./db/database");
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
+app.use(
+  cors({
+    credentials: true,
+    origin: true,
+    allowedHeaders: "*",
+  })
+);
+
 app.use(
   cors({
     origin: [
